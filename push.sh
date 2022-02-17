0c6e7b52be39739da76f41edab0688b229287122
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-17 16:05:15 +0530

    add resume using script

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-TOKEN=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
 
+# Set the github token using `export GITHUB_TOKEN="ghp_NUpU9xxtwrrq9Tz2nLx2Rc9Rjh3axxxxxxxx"`
 git add *
 git branch -M main
 git commit -m "add resume using script"
