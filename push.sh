bc1b832620729da4e25c11791c02e3cde7e1cb88
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-17 16:47:12 +0530

    add resume using script

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Set the github token using `export GITHUB_TOKEN="ghp_NUpU9xxtwrrq9Tz2nLx2Rc9Rjh3axxxxxxxx"`
-git add *
+git add * saved_data/*
 git commit -m "add resume using script"
 git push https://subhayu99:$GITHUB_TOKEN@github.com/subhayu99/resume_builder_api.git
