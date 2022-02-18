ee86ac2a7677b5468547a56251fc3d7aea32c49e
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-18 15:29:52 +0530

    added submodule

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set the github token using `export GITHUB_TOKEN="ghp_NUpU9xxtwrrq9Tz2nLx2Rc9Rjh3axxxxxxxx"`
-git pull
-git add *
+cd givemyresume.github.io
+git add .
 git commit -m "add resume using script"
-git push https://subhayu99:$GITHUB_TOKEN@github.com/subhayu99/resume_builder_api.git
+git push https://subhayu99:$GITHUB_TOKEN@github.com/givemyresume/givemyresume.github.io.git
