86dc1252d74d7dfe2f9e68732f586153c1b00640
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-23 20:49:12 +0000

    add resume using script

--- src/sendemail.py
+++ src/sendemail.py
@@ -7,8 +7,8 @@ def sendmail(to, url, name):
     subject = "Hooray!!! Your resume has been created"
     smtpserver = smtplib.SMTP("smtp.givemyresume.tech:587")
     smtpserver.login(sender, password)
-    header = f'To:{to}\nFrom:{sender}\nSubject:{subject}\n'
     print(to, url, name)
+    header = f'To:{to}\nFrom:{sender}\nSubject:{subject}\n'
     msg = header + f'\n Hello, {name}\n\n You can now view your resume at {url}\n\n'
     smtpserver.sendmail(sender, to, msg)
     smtpserver.close()
