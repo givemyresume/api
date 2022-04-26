8e2a39df2e0cf8ee7d516123c8eb52c61315dc1d
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-04-26 20:20:57 +0000

    add resume using script

--- auth/model.py
+++ auth/model.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
+from fastapi import Query
 
 class User(BaseModel):
-    username: str
-    password: str
-    email: str
+    username: str = Query("", min_length=4, max_length=16, regex="^[a-zA-Z0-9]{4,16}$")
+    password: str = Query("", min_length=8, regex="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+    email: str = Query("", min_length=8, regex="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,6}$")
 class Token(BaseModel):
     access_token: str
     token_type: str
